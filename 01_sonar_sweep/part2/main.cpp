73bd86b1ec31067ea0a47345a565972b3ec0ec91
Tony Bisegna <tony.bisegna@amadeus.com>
2021-12-16 21:14:24 +0100

    Clean and day 1

--- 01_sonar_sweep/part2/main.cpp
+++ 01_sonar_sweep/part2/main.cpp
@@ -8,22 +8,27 @@ int main() {
     cin.tie(NULL); cout.tie(NULL);
     
     uint aMeasure;
+    uint idx=0
     vector<uint> aMesures;
+    aMesures.resize(3000,0);
     while (cin>>aMeasure)
     {
-        aMesures.push_back(aMeasure);
+        aMesures[idx] += aMeasure;
+        aMesures[idx+1] += aMeasure;
+        aMesures[idx+2] += aMeasure;
+        ++idx;
     }
-    
+    aMesures.resize(idx-1);
+
     vector<uint> aWindowedMesures;
     for (int i=0 ; i<aMesures.size()-2 ; ++i)
     {
         aWindowedMesures.push_back(aMesures[i] + aMesures[i+1] + aMesures[i+2]);
-        if (i<5) dbg(aWindowedMesures.back());
     }    
 
     uint aRes=0;
     uint aPrev = -1;
-    for(auto aWin : aWindowedMesures){
+    for(auto aWin : aMesures){
         if (aWin > aPrev)
         {
             ++aRes;
